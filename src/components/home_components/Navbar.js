26237376ce61291277fa41e23649eb13036c6511
pkc189 <pankajchauhan189@gmail.com>
2020-12-16 18:42:17 +0530

    4 files modified, 1 added

--- src/components/home_components/Navbar.js
+++ src/components/home_components/Navbar.js
@@ -4,38 +4,48 @@ import { IconContext } from "react-icons";
 import "../../css/Navbar.css";
 import { AiOutlineSearch } from "react-icons/ai";
 
+import { Link, BrowserRouter as Router } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
-      <div className="Navbar">
-        <IconContext.Provider
-          value={{ color: "F9D56E", className: "Navbar__icon", size: "1.5rem" }}
-        >
-          <FaHome />
-        </IconContext.Provider>
+      <Router>
+        <div className="Navbar">
+          <IconContext.Provider
+            value={{
+              color: "5aa469",
+              className: "Navbar__icon",
+              size: "1.5rem"
+            }}
+          >
+            <Link to="/">
+              <FaHome />
+            </Link>
+          </IconContext.Provider>
 
-        <div id="search">
-          <form method="get" action="">
-            <div className="search__input">
-              <input type="text" placeholder="Search" required />
+          <div id="search">
+            <form method="get" action="">
+              <div className="search__input">
+                <input type="text" placeholder="Search" required />
 
-              <div className="search__input__button">
-                <button type="submit">
-                  <IconContext.Provider
-                    value={{
-                      color: "92929D",
-                      className: "Search__icon",
-                      size: "1.2rem"
-                    }}
-                  >
-                    <AiOutlineSearch />
-                  </IconContext.Provider>
-                </button>
+                <div className="search__input__button">
+                  <button type="submit">
+                    <IconContext.Provider
+                      value={{
+                        color: "5aa469",
+                        className: "Search__icon",
+                        size: "1.6rem"
+                      }}
+                    >
+                      <AiOutlineSearch />
+                    </IconContext.Provider>
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
+      </Router>
     </>
   );
 };
