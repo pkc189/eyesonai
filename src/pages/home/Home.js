15f45e1ca9c3aeae0aece606ac6787cbadbaf846
pkc189 <pankajchauhan189@gmail.com>
2020-12-16 22:57:25 +0530

    1 file modified, 6 added, 3 deleted

---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../pages/global/navbar/Navbar.js";
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h2>Homen</h2>
+    </div>
+  );
+}
