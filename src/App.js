fdb7a15ab21fb2adb38b6ded55c169d89a980945
pkc189 <pankajchauhan189@gmail.com>
2020-12-16 22:34:47 +0530

    1 file modified, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Home from "./pages/Home.js";
 import "./style.css";
-import Navbar from "./components/home_components/Navbar.js";
 
 export default function App() {
   return (
     <div>
-      <Navbar />
       <Home />
     </div>
   );
